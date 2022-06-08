c38c340ec52060d0a2b734373348eeaee9556994
Wavelopper <alnetweb59@gmail.com>
2022-06-08 17:01:33 +0200

    [UPDATE]

---
+++ assets/class/game.js
@@ -0,0 +1,71 @@
+import Dresseur from "./dresseur.js";
+export default class Game {
+    constructor() {
+        this.listPlayer = [];
+        this.turnPlayer = 0;
+        const player1 = new Dresseur({
+            name: "Player1",
+            color: "blue",
+        });
+        const player2 = new Dresseur({
+            name: "IA",
+            color: "red",
+        });
+        this.listPlayer.push(player1, player2);
+        initGame(this.listPlayer);
+        
+        const getCurrentTurnPlayer = () => {
+            return this.listPlayer[this.turnPlayer];
+        };
+        
+    }
+    
+}
+
+const body = document.querySelector("body"); 
+let container = document.querySelector("#container");
+let battleButton = document.querySelector("#battle");
+const choices = document.querySelectorAll(".choice__player");
+const displayChoiceIa = (choice)=>{
+    console.log(choice);
+    let divIa = document.createElement("div");
+    divIa.id = "divIa";
+    let img = document.createElement("img");
+
+    switch(choice){
+        case "Feu": 
+        img.src = "./assets/img/salameche.png";
+        break;
+        case "Eau": 
+        img.src = "./assets/img/carapuce.png";
+        break;
+        case "Plante": 
+        img.src = "./assets//img/bulbizarre.png";
+        break;
+    }
+    battleButton.after(divIa);
+    divIa.appendChild(img);
+}
+
+const initGame = (list) => {
+    console.log(list);
+    let choiceIa = getRandomInt(list[1].choice.length);
+    let choicePlayer = list[1].choice = list[1].choice[choiceIa]; 
+    displayChoiceIa(choicePlayer);
+    getChoicePlayer(choices);
+}
+
+
+const getChoicePlayer = (choices) => {
+ console.log(choices);
+} 
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+
+
+
+
+
---
+++ assets/class/main.js
@@ -0,0 +1,5 @@
+import Game from "./game.js";
+
+window.addEventListener("load", () => {
+    window.Game = new Game();
+});
