fc00003a301d4ae9d2bbc0ac52e65a7d19d85d74
Wavelopper <alnetweb59@gmail.com>
2022-06-09 06:06:49 +0100

    [UPDATE] egality

--- assets/js/class/dresseur.js
+++ assets/js/class/dresseur.js
@@ -1,6 +1,5 @@
 export default class Dresseur{
     constructor(){
-        this.name = name;
         this.choice = ["Eau", "Feu", "Plante"];
         this.nbPoint = 0;
         this.turnPlayer = 0;
--- assets/js/class/game.js
+++ assets/js/class/game.js
@@ -1,75 +1,92 @@
 import Dresseur from "./dresseur.js";
 export default class Game {
-    constructor() {
-        this.listPlayer = [];
-        this.turnPlayer = 0;
-        const player1 = new Dresseur({
-            name: "Player1",
-            color: "blue",
-        });
-        const player2 = new Dresseur({
-            name: "IA",
-            color: "red",
-        });
-        this.listPlayer.push(player1, player2);
-        initGame(this.listPlayer);
-        
-        const getCurrentTurnPlayer = () => {
-            return this.listPlayer[this.turnPlayer];
-        };
-        
-    }
+  constructor() {
+    this.listPlayer = [];
+    this.turnPlayer = 0;
+    const player1 = new Dresseur({
+      name: "Blue",
+      color: "blue",
+    });
+    const player2 = new Dresseur({
+      name: "Red",
+      color: "red",
+    });
+    this.listPlayer.push(player1, player2);
+    initGame(this.listPlayer);
+
+    const getCurrentTurnPlayer = () => {
+      return this.listPlayer[this.turnPlayer];
+    };
     
+  }
 }
 
-const body = document.querySelector("body"); 
+const body = document.querySelector("body");
 let container = document.querySelector("#container");
 let battleButton = document.querySelector("#battle");
 const choices = document.querySelectorAll(".choice__player");
-const displayChoiceIa = (choice)=>{
-    console.log(choice);
-    let divIa = document.createElement("div");
-    divIa.id = "divIa";
-    let img = document.createElement("img");
 
-    switch(choice){
-        case "Feu": 
-        img.src = "./assets/img/salameche.png";
-        break;
-        case "Eau": 
-        img.src = "./assets/img/carapuce.png";
-        break;
-        case "Plante": 
-        img.src = "./assets//img/bulbizarre.png";
-        break;
-    }
-    battleButton.after(divIa);
-    divIa.appendChild(img);
-}
+
+const displayChoiceIa = (choice, list) => {
+  console.log(`Choix de l'IA :  ${choice}`);
+  let divIa = document.createElement("div");
+  divIa.id = "divIa";
+  let img = document.createElement("img");
+
+  switch (choice) {
+    case "Feu":
+      img.src = "./assets/img/salameche.png";
+      break;
+    case "Eau":
+      img.src = "./assets/img/carapuce.png";
+      break;
+    case "Plante":
+      img.src = "./assets//img/bulbizarre.png";
+      break;
+  }
+  battleButton.after(divIa);
+  divIa.appendChild(img);
+};
 
 const initGame = (list) => {
-    console.log(list);
-    let choiceIa = getRandomInt(list[1].choice.length);
-    let choicePlayer = list[1].choice = list[1].choice[choiceIa]; 
-    displayChoiceIa(choicePlayer);
-    getChoicePlayer(choices, list);
-}
+  let choiceIa = getRandomInt(list[1].choice.length);
+  let choicePlayer2 = (list[1].choice = list[1].choice[choiceIa]);
+  displayChoiceIa(choicePlayer2, list);
+  getChoicePlayer(choices, list);
+
+  battleButton.addEventListener("click", ()=>{
+    battle(list[0].choice, list[1].choice, list);
+})
+  
+};
+
 
 
 const getChoicePlayer = (choices, list) => {
-     choices.forEach(item => {
-         item.addEventListener("click", () =>{
-             list[0].choice = item.id;
-         })
-     });
-} 
+  choices.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      let finalChoice = item.id;
+      console.log("Choix du joueur : " + finalChoice);
+      list[0].choice = finalChoice;
+      document.getElementById(finalChoice).classList.add("highlight");
+    });
+  });
+};
+
+const removeColor = () => {};
+
+const battle = (player1, player2, list) => {
+    if(player1 == player2){
+        console.log("égalité");
+    }
+
+};
+
+
+
 
 const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-}
-
-
-
-
+  return Math.floor(Math.random() * max);
+};
 
 
