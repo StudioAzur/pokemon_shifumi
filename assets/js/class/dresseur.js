cf2123698f517a6d26d8de3d3fb14b839f1711fb
Wavelopper <alnetweb59@gmail.com>
2022-06-20 00:53:02 +0200

    [STRUCTURE] optimise object and class concept

--- assets/js/class/dresseur.js
+++ assets/js/class/dresseur.js
@@ -1,9 +1,13 @@
 /* La classe Dresseur a un constructeur qui ne prend aucun argument et initialise les variables
 d'instance choice et nbPoint à la chaîne vide et 0, respectivement. */
 export default class Dresseur{
-    constructor(){
+    constructor(name, color){
+        this.name = name;
+        this.color = color;
         this.choice = "";
-        this.nbPoint = 0;
+        this.score = 0;
     }
-    
 }
+
+const player1 = new Dresseur("Blue", "blue");
+const player2 = new Dresseur("Red", "red");
--- assets/js/class/game.js
+++ assets/js/class/game.js
@@ -2,182 +2,167 @@ import Dresseur from "./dresseur.js";
 /* La classe Game est une classe qui contient une liste de joueurs et une fonction qui renvoie le
 joueur actuel. */
 export default class Game {
-    constructor() {
-        this.listPlayer = [];
-        const player1 = new Dresseur({
-            name: "Blue",
-            color: "blue",
-        });
-        const player2 = new Dresseur({
-            name: "Red",
-            color: "red",
-        });
-        this.listPlayer.push(player1, player2);
-        initGame(this.listPlayer);
+  constructor() {
+    this.params = {
+      listPlayer: [],
+      battleButton: document.querySelector("#battle"),
+      choices: document.querySelectorAll(".choice__player"),
+      restart: document.querySelector("#restart"),
+      elements: ["Feu", "Eau", "Plante"],
+    };
+    this.player = {
+      blue: new Dresseur("Blue", "blue"),
+      red: new Dresseur("Red", "red"),
+    };
+    this.params.listPlayer.push(this.player.blue, this.player.red);
+    this.initGame(this.params.listPlayer);
+  }
 
-        const getCurrentTurnPlayer = () => {
-            return this.listPlayer[this.turnPlayer];
-        };
-    }
-}
+  /**
+   * Cette fonction initialise le jeu en obtenant le choix du joueur, en ajoutant un écouteur d'événement
+   * au bouton de combat et en affichant le score.
+   * @param list - un tableau d'objets
+   */
+  initGame = (list) => {
+    let winner = "";
+    this.displayScore();
+    this.getChoicePlayer(this.params.choices, list);
+    this.params.battleButton.addEventListener("click", () => {
+      let choiceIa = this.getRandomInt(this.params.elements.length);
+      this.player.red.choice = this.params.elements[choiceIa];
+      this.displayChoiceIa(this.player.red.choice);
+      this.battle(this.player.blue.choice, this.player.red.choice);
 
-let battleButton = document.querySelector("#battle");
-const choices = document.querySelectorAll(".choice__player");
-let restart = document.querySelector("#restart");
+      if (this.player.blue.score == 2 || this.player.red.score == 2) {
+        if (this.player.blue.score == 2) {
+          winner = this.player.blue.name;
+        } else {
+          winner = this.player.red.name;
+        }
+        this.gameOver(winner);
+      }
+    });
+  };
 
-restart.addEventListener("click", () => {
-    location.reload();
-});
-
-/**
- * Cette fonction supprime l'image, crée une nouvelle image et enregistre le choix de l'IA.
- * @param choice - le choix de l'IA
- * @param list - la liste des choix
- */
-const displayChoiceIa = (choice) => {
-    removeImg();
-    createImage(choice);
+  /**
+   * Cette fonction supprime l'image, crée une nouvelle image et enregistre le choix de l'IA.
+   * @param choice - le choix de l'IA
+   * @param list - la liste des choix
+   */
+  displayChoiceIa = (choice) => {
+    this.removeImg();
+    this.createImage(choice);
     console.log(`Choix de l'IA :  ${choice}`);
-};
+  };
 
-/**
- * Il crée un élément d'image, définit son id et src, et l'ajoute à la div avec l'id divIa.
- * @param choice - le choix de l'IA
- */
-const createImage = (choice) => {
+  /**
+   * Il crée un élément d'image, définit son id et src, et l'ajoute à la div avec l'id divIa.
+   * @param choice - le choix de l'IA
+   */
+  createImage = (choice) => {
     let img = document.createElement("img");
     let divIa = document.querySelector("#divIa");
     img.id = "choiceIa";
     switch (choice) {
-        case "Feu":
-            img.src = "./assets/img/salameche.png";
-            break;
-        case "Eau":
-            img.src = "./assets/img/carapuce.png";
-            break;
-        case "Plante":
-            img.src = "./assets//img/bulbizarre.png";
-            break;
+      case "Feu":
+        img.src = "./assets/img/salameche.png";
+        break;
+      case "Eau":
+        img.src = "./assets/img/carapuce.png";
+        break;
+      case "Plante":
+        img.src = "./assets//img/bulbizarre.png";
+        break;
     }
     divIa.appendChild(img);
-};
+  };
 
-/**
- * Cette fonction initialise le jeu en obtenant le choix du joueur, en ajoutant un écouteur d'événement
- * au bouton de combat et en affichant le score.
- * @param list - un tableau d'objets
- */
-const initGame = (list) => {
-    const elements = ["Feu", "Eau", "Plante"];
-    getChoicePlayer(choices, list);
-    battleButton.addEventListener("click", () => {
-        let choiceIa = getRandomInt(elements.length);
-        let choicePlayer2;
-        choicePlayer2 = list[1].choice = elements[choiceIa];
-        displayChoiceIa(choicePlayer2);
-        battle(list[0].choice, list[1].choice, list);
-
-        if (list[0].nbPoint == 2 || list[1].nbPoint == 2) {
-            let winner;
-            if (list[0].nbPoint == 2) {
-                winner = "Blue";
-            } else {
-                winner = "Red";
-            }
-            gameOver(winner);
-        }
-        displayScore(list);
-    });
-};
-
-/**
- * le joueur perd la partie.
- */
-const gameOver = (winner) => {
+  /**
+   * le joueur perd la partie.
+   */
+  gameOver = (winner) => {
     let result = document.querySelector("#result");
     result.textContent = `${winner} a gagné !`;
-    battleButton.disabled = "true";
-    restart.style.visibility = "visible";
-};
+    this.params.battleButton.disabled = "true";
+    this.params.restart.style.display = "block";
+    this.params.restart.addEventListener("click", this.restart);
+  };
 
-/**
- * Il ajoute un écouteur d'événement à chacun des trois choix, et lorsque l'un d'eux est cliqué, il
- * supprime la classe de surbrillance de tous les choix et l'ajoute à celui qui a été cliqué.
- * @param choices - la liste des choix (pierre, papier, ciseaux)
- * @param list - un tableau contenant le choix du joueur et le choix de l'ordinateur
- */
-const getChoicePlayer = (choices, list) => {
+  /* Rechargement de la page. */
+  restart = () => {
+    window.location.reload();
+  };
+
+  /* Une fonction qui prend un tableau de choix et ajoute un écouteur d'événement à chaque élément du
+  tableau. */
+  getChoicePlayer = (choices) => {
     choices.forEach((item) => {
-        item.addEventListener("click", (e) => {
-            let finalChoice = item.id;
-            console.log("Choix du joueur : " + finalChoice);
-            list[0].choice = finalChoice;
-            let highlight = document.querySelectorAll(".highlight");
-            highlight.forEach((element) => {
-                element.classList.remove("highlight");
-            });
-            document.getElementById(finalChoice).classList.add("highlight");
-        });
+      item.addEventListener("click", (e) => {
+        let finalChoice = item.id;
+        console.log("Choix du joueur : " + finalChoice);
+        this.player.blue.choice = finalChoice;
+        this.highlight(finalChoice);
+      });
     });
-};
+  };
 
-/**
- * Il supprime l'image de la div avec l'id de "divIa".
- */
-const removeImg = () => {
+  /* Suppression de la classe de surbrillance de tous les éléments avec la classe de surbrillance et
+  ajout de la classe de surbrillance à l'élément avec l'id de finalChoice. */
+  highlight = (finalChoice) => {
+    let highlight = document.querySelectorAll(".highlight");
+    highlight.forEach((element) => {
+      element.classList.remove("highlight");
+    });
+    document.getElementById(finalChoice).classList.add("highlight");
+  };
+  /**
+   * Il supprime l'image de la div avec l'id de "divIa".
+   */
+  removeImg = () => {
     let img = document.querySelector("#divIa");
     img.innerHTML = "";
-};
+  };
 
-/**
- * La fonction prend trois arguments, dont deux sont des chaînes et le troisième est un tableau. La
- * fonction compare les deux chaînes et si elles sont égales, elle définit le textContent de l'élément
- * avec l'id de "result" sur "Egalité". Si les chaînes ne sont pas égales, il les compare à un ensemble
- * de trois conditions et si l'une de ces conditions est remplie, il définit le textContent de
- * l'élément avec l'id de "result" sur "Vous avez gagné !!" et incrémente de un la propriété nbPoint du
- * premier objet du tableau. Si aucune des conditions n'est remplie, il définit le textContent de
- * l'élément avec l'id de "result" sur "Vous avez perdu !" et incrémente de un la propriété nbPoint du
- * deuxième objet du tableau.
- * @param player1 - le choix du joueur
- * @param player2 - "Plante"
- * @param list - [{
- */
-const battle = (player1, player2, list) => {
+  /* Une fonction qui compare le choix du joueur et le choix de l'IA. */
+  battle = () => {
     let result = document.querySelector("#result");
     let textResult;
-    if (player1 == player2) {
-        result.textContent = "Egalité";
+    if (this.player.blue.choice == this.player.red.choice) {
+      result.textContent = "Egalité";
     } else if (
-        (player1 === "Feu" && player2 === "Plante") ||
-        (player1 === "Eau" && player2 === "Feu") ||
-        (player1 === "Plante" && player2 === "Eau")
+      (this.player.blue.choice === "Feu" &&
+        this.player.red.choice === "Plante") ||
+      (this.player.blue.choice === "Eau" && this.player.red.choice === "Feu") ||
+      (this.player.blue.choice === "Plante" && this.player.red.choice === "Eau")
     ) {
-        textResult = "Vous avez gagné !!";
-        result.textContent = textResult;
-        list[0].nbPoint++;
+      textResult = "Vous avez gagné !!";
+      result.textContent = textResult;
+      this.player.blue.score++;
     } else {
-        result.textContent = "Vous avez perdu !";
-        list[1].nbPoint++;
+      result.textContent = "Vous avez perdu !";
+      this.player.red.score++;
     }
-};
+    this.displayScore();
+  };
 
-/**
- * Il prend une liste d'objets, puis il définit le textContent de deux éléments sur les valeurs de deux
- * propriétés de deux objets de la liste.
- * @param list - nbPoint
- */
-const displayScore = (list) => {
+  /**
+   * Il prend une liste d'objets, puis il définit le textContent de deux éléments sur les valeurs de deux
+   * propriétés de deux objets de la liste.
+   * @param list - nbPoint
+   */
+  displayScore = () => {
     let blue = document.querySelector("#score_blue");
     let red = document.querySelector("#score_red");
-    red.textContent = list[1].nbPoint;
-    blue.textContent = list[0].nbPoint;
-};
+    blue.textContent = this.player.blue.score;
+    red.textContent = this.player.red.score;
+  };
 
-/**
- * Il renvoie un entier aléatoire entre 0 et le nombre passé.
- * @param max - Le nombre maximum que vous souhaitez générer.
- * @returns Une fonction qui prend un paramètre et renvoie un nombre aléatoire entre 0 et le paramètre.
- */
-const getRandomInt = (max) => {
+  /**
+   * Il renvoie un entier aléatoire entre 0 et le nombre passé.
+   * @param max - Le nombre maximum que vous souhaitez générer.
+   * @returns Une fonction qui prend un paramètre et renvoie un nombre aléatoire entre 0 et le paramètre.
+   */
+  getRandomInt = (max) => {
     return Math.floor(Math.random() * max);
-};
+  };
+}
